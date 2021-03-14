dedd0c7686f1528c5ddf287f6faefd751ca01b0b
kunalreddy <kunalreddyaleti@gmail.com>
2021-03-14 10:12:09 -0500

    Fixing Errors

--- dataframe/Exercise.py
+++ dataframe/Exercise.py
@@ -46,9 +46,10 @@ if __name__ == '__main__':
     print("Printing DataFrame " )
     data_df.show(8)
     data_df.select(to_timestamp('action_at').alias('Time')).show()
-    data2_df=data_df.groupBy("ride_id").pivot("action_type").agg(first(to_timestamp("action_at")))
-    data2_df.select(when('cancel_ride'=='null',datediff('payment','request_ride'))\
-                    .otherwise(datediff('payment', 'cancel_ride'))).show()
-    ## spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" dataframe/Exercise.py
+    data2_df=data_df.groupBy("passenger_id", "ride_id").pivot("action_type").agg(first(to_timestamp("action_at")))
+    data2_df.select("*", when(col('cancel_ride').isNull(),(col('payment').cast("long") - col('request_ride').cast("long"))/60)\
+                    .otherwise((col('cancel_ride').cast("long") - col('request_ride').cast("long"))/60)).show()
+
+## spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" dataframe/Exercise.py
 
 
